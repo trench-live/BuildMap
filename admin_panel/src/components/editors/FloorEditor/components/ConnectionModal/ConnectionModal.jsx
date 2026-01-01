fe24a13e7dfba3ad4f32eff631b9e317993925e3
Artem <artemalesin89778@gmail.com>
2026-01-01 15:51:50 +0300

    fix/admin_panel -> fixed strings format

--- admin_panel/src/components/editors/FloorEditor/components/ConnectionModal/ConnectionModal.jsx
+++ admin_panel/src/components/editors/FloorEditor/components/ConnectionModal/ConnectionModal.jsx
@@ -130,10 +130,10 @@ const ConnectionModal = ({
                                     onChange={(e) => updateField('bidirectional', e.target.checked)}
                                 />
                                 <span className="checkbox-text">
-                                    {decodeURIComponent('%D0%94%D0%B2%D1%83%D0%BD%D0%B0%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%81%D0%B2%D1%8F%D0%B7%D1%8C')}
+                                    {' Двунаправленная связь'}
                                 </span>
                             </label>
-                            <span className="hint">{decodeURIComponent('%D0%90%D0%B2%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%20%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C/%D0%BE%D0%B1%D0%BD%D0%BE%D0%B2%D0%B8%D1%82%D1%8C%20%D0%BE%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D1%83%D1%8E%20%D1%81%D0%B2%D1%8F%D0%B7%D1%8C.')}</span>
+                            <span className="hint">{'Автоматически создать/обновить обратную связь.'}</span>
                         </div>
                     </ModalContent>
 
